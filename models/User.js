c51c4692161fdf1c87d3b414f91991c11a14bc32
aman-alem <alem.aman@gmail.com>
2022-06-07 13:28:18 -0500

    User model created

---
+++ models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+require('./List')
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    list: [listSchema],
+    googleId: String
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', userSchema);
